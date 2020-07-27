285f727d1432379863a7e594e4289fb4d728b4e2
Nicolas Elena <37024797+NicolasElena@users.noreply.github.com>
2020-07-27 19:50:50 -0300

    Setup inicial do projeto

---
+++ src/components/Footer/index.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { FooterBase } from "./styles";
+
+function Footer() {
+  return (
+    <FooterBase>
+      <a href="https://www.alura.com.br/">
+        <img
+          src="https://www.alura.com.br/assets/img/alura-logo-white.1570550707.svg"
+          alt="Logo Alura"
+        />
+      </a>
+      <p>
+        This site is fan-made and not affiliated with Grinding Gear Games in any
+        way.
+      </p>
+      <p>
+        Proudly build during{" "}
+        <a href="https://www.alura.com.br/">Imersão React da Alura</a>
+      </p>
+    </FooterBase>
+  );
+}
+
+export default Footer;
