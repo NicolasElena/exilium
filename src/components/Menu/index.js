14b30216c6f6ce602e1c9f4679a8a6fd3e650f22
Nicolas Elena <37024797+NicolasElena@users.noreply.github.com>
2020-07-29 14:02:02 -0300

    Alterações no roteamento, import react-slick

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,17 +1,17 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import Logo from "../../assets/img/Logo.png";
-//import "./Menu.css";
 import { LogoImage, MenuWrapper } from "./style.js";
 import Button from "../Button";
 
 function Menu() {
   return (
     <MenuWrapper className="Menu">
-      <a href="/">
-        <LogoImage className="Logo" src={Logo} alt="PathFlix" />
-      </a>
+      <Link to="/">
+        <LogoImage className="Logo" src={Logo} alt="Exilium" />
+      </Link>
 
-      <Button as="a" className="ButtonLink" href="/">
+      <Button as={Link} className="ButtonLink" to="/cadastro/video">
         Novo Vídeo
       </Button>
     </MenuWrapper>
